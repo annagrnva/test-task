95ab5f90ba2bd6c2096f40b3e0c83092011e46a1
annagrnva <m-ippolitova@mail.ru>
2025-05-02 19:14:02 +0400

    pagination arrows

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,15 +2,43 @@ import styles from "./style.module.scss";
 import arrowLeft from "../../assets/images/arrow-left-pagination.svg";
 import arrowRight from "../../assets/images/arrow-right-pagination.svg";
 import { Paragraph } from "../Typography";
+import { useState } from "react";
 
 export const Pagination = () => {
+  const totalPages = 9;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const previousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <>
       <Paragraph type="bigLight">
         <div className={styles.pagination}>
-          <img src={arrowLeft} alt="arrowLeft" />
-          <div className={styles.number}>1</div>
-          <img src={arrowRight} alt="arrowRight" />
+          <img
+            src={arrowLeft}
+            alt="arrowLeft"
+            onClick={previousPage}
+            // className={styles.pointer}
+          />
+
+          <div className={styles.number}>{currentPage}</div>
+
+          <img
+            src={arrowRight}
+            alt="arrowRight"
+            onClick={nextPage}
+            // className={styles.pointer}
+          />
         </div>
       </Paragraph>
     </>
